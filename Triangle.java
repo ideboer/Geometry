275fec070c524baf40252ba0457b510d31ec0097
Jack Aidan Hickisch <hickischja@s.dcsdk12.org>
2020-01-15 08:56:53 -0700

    Tringle done

--- Triangle.java
+++ Triangle.java
@@ -1,12 +1,12 @@
 import java.util.*;
 
-class Triange
+class Triangle
 {
     Point a;
     Point b;
     Point c;
 
-    public Trangle(Point a, Point b, Point c)
+    public Triangle(Point a, Point b, Point c)
     {
         this.a = a;
         this.b = b;
