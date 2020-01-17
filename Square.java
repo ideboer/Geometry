8c1d0daf0ddf09bcc79e3f40c415213535ad0556
Jack Aidan Hickisch <hickischja@s.dcsdk12.org>
2020-01-17 08:25:42 -0700

    working on static function

--- Square.java
+++ Square.java
@@ -1,18 +1,18 @@
 import java.util.*;
 
 class Square {
-    Point ll;
-    Point ul;
-    Point lr;
-    Point up;
-    double length;
+    private Point ll;
+    private Point ul;
+    private Point lr;
+    private Point ur;
+    private double length;
 
     public Square(Point ll, double length) {
         this.ll = ll;
         this.length = length;
-        ul = new Point(a.getX(), a.getY() + length);
-        lr = new Point(a.getX() + length;, a.getY());
-        ul = new Point(a.getX() + length, a.getY() + length);
+        ul = new Point(ll.getX(), ll.getY() + length);
+        lr = new Point(ll.getX() + length, ll.getY());
+        ur = new Point(ll.getX() + length, ll.getY() + length);
     }
 
     public double area() {
@@ -21,5 +21,68 @@ class Square {
 
     public double perimeter() {
         return length * 4;
+    }
+
+    public Point getLL()
+    {
+        return ll;
+    }
+    public Point getUL()
+    {
+        return ul;
+    }
+    public Point getLR()
+    {
+        return lr;
+    }
+    public Point getUR()
+    {
+        return ur;
+    }
+
+
+    public static boolean isOverlapping(Square a, Square b) {
+        if (a.getLL().getY() == b.getLL().getY() && a.getLL().getX() == b.getLL().getX())
+        {
+            return true;
+        }
+        else if (a.getLL().getY() == b.getUL().getY() && a.getLL().getX() == b.getUL().getX())
+        {
+            return true;
+        }
+        else if (a.getLL().getY() == b.getUR().getY() && a.getLL().getX() == b.getUR().getX())
+        {
+            return true;
+        }
+        else if (a.getLL().getY() == b.getLR().getY() && a.getLL().getX() == b.getLR().getX())
+        {
+            return true;
+        }
+
+        
+        else if (a.getLL().getY() == b.getLL().getY() && a.getLL().getX() == b.getLL().getX())
+        {
+            return true;
+        }
+        else if (a.getLL().getY() == b.getLL().getY() && a.getLL().getX() == b.getLL().getX())
+        {
+            return true;
+        }
+        else if (a.getLL().getY() == b.getLL().getY() && a.getLL().getX() == b.getLL().getX())
+        {
+            return true;
+        }
+        else if (a.getLL().getY() == b.getLL().getY() && a.getLL().getX() == b.getLL().getX())
+        {
+            return true;
+        }
+        else if (a.getLL().getY() == b.getLL().getY() && a.getLL().getX() == b.getLL().getX())
+        {
+            return true;
+        }
+        else if (a.getLL().getY() == b.getLL().getY() && a.getLL().getX() == b.getLL().getX())
+        {
+            return true;
+        }
     }
 }
