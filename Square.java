6fadb2b573fbd776a7842dbb249e8b556c246f59
Jack Aidan Hickisch <hickischja@s.dcsdk12.org>
2020-01-21 08:20:45 -0700

    Square intersect

--- Square.java
+++ Square.java
@@ -42,47 +42,100 @@ class Square {
 
 
     public static boolean isOverlapping(Square a, Square b) {
-        if (a.getLL().getY() == b.getLL().getY() && a.getLL().getX() == b.getLL().getX())
+        // if      (a.getLL().getY() == b.getLL().getY() && a.getLL().getX() == b.getLL().getX())
+        // {
+        //     return true;
+        // }
+        // else if (a.getLL().getY() == b.getUL().getY() && a.getLL().getX() == b.getUL().getX())
+        // {
+        //     return true;
+        // }
+        // else if (a.getLL().getY() == b.getUR().getY() && a.getLL().getX() == b.getUR().getX())
+        // {
+        //     return true;
+        // }
+        // else if (a.getLL().getY() == b.getLR().getY() && a.getLL().getX() == b.getLR().getX())
+        // {
+        //     return true;
+        // }
+        // else if (a.getUL().getY() == b.getUL().getY() && a.getUL().getX() == b.getUL().getX())
+        // {
+        //     return true;
+        // }
+        // else if (a.getUL().getY() == b.getUR().getY() && a.getUL().getX() == b.getUR().getX())
+        // {
+        //     return true;
+        // }
+        // else if (a.getUL().getY() == b.getLR().getY() && a.getUL().getX() == b.getLR().getX())
+        // {
+        //     return true;
+        // }
+        // else if (a.getUR().getY() == b.getUR().getY() && a.getUR().getX() == b.getUR().getX())
+        // {
+        //     return true;
+        // }
+        // else if (a.getUR().getY() == b.getLR().getY() && a.getUR().getX() == b.getLR().getX())
+        // {
+        //     return true;
+        // }
+        // else if (a.getLR().getY() == b.getLR().getY() && a.getLR().getX() == b.getLR().getX())
+        // {
+        //     return true;
+        // }
+
+
+
+        // if (a.getUL().getX() <= b.getUL().getX() && a.getLR().getX() >= b.getUL().getX() && a.getUL().getY() >= b.getUL().getY() && a.getLR().getX() <= b.getUL().getY())
+        // {
+        //     return true;
+        // }
+        // if (a.getUL().getX() <= b.getUR().getX() && a.getLR().getX() >= b.getUR().getX() && a.getUL().getY() >= b.getUR().getY() && a.getLR().getX() <= b.getUR().getY())
+        // {
+        //     return true;
+        // }
+        // if (a.getUL().getX() <= b.getLR().getX() && a.getLR().getX() >= b.getLR().getX() && a.getUL().getY() >= b.getLR().getY() && a.getLR().getX() <= b.getLR().getY())
+        // {
+        //     return true;
+        // }
+        // if (a.getUL().getX() <= b.getLL().getX() && a.getLR().getX() >= b.getLL().getX() && a.getUL().getY() >= b.getLL().getY() && a.getLR().getX() <= b.getLL().getY())
+        // {
+        //     return true;
+        // }
+
+        // return false;
+
+
+
+
+        double[] bXCords = new double[b.getUL().getX(), b.getUR().getX(), b.getLR().getX(), b.getLL().getX()];
+        double[] bYCords = new double[b.getUL().getY(), b.getUR().getY(), b.getLR().getY(), b.getLL().getY()];
+
+
+        for (int i = 0; i < 4; i++)
         {
-            return true;
-        }
-        else if (a.getLL().getY() == b.getUL().getY() && a.getLL().getX() == b.getUL().getX())
-        {
-            return true;
-        }
-        else if (a.getLL().getY() == b.getUR().getY() && a.getLL().getX() == b.getUR().getX())
-        {
-            return true;
-        }
-        else if (a.getLL().getY() == b.getLR().getY() && a.getLL().getX() == b.getLR().getX())
-        {
-            return true;
+            if (a.getUL().getX() <= bXCords[i] && a.getLR().getX() >= bXCords[i] && a.getUL().getY() >= bYCords[i] && a.getLR().getX() <= bYCords[i])
+            {
+                return true;
+            }
         }
 
-        
-        else if (a.getLL().getY() == b.getLL().getY() && a.getLL().getX() == b.getLL().getX())
+        return false;
+    }
+
+    public boolean isOverlapping(Square b)
+    {
+        double[] bXCords = new double[b.getUL().getX(), b.getUR().getX(), b.getLR().getX(), b.getLL().getX()];
+        double[] bYCords = new double[b.getUL().getY(), b.getUR().getY(), b.getLR().getY(), b.getLL().getY()];
+
+
+        for (int i = 0; i < 4; i++)
         {
-            return true;
+            if (ul.getX() <= bXCords[i] && lr.getX() >= bXCords[i] && ul.getY() >= bYCords[i] && lr.getX() <= bYCords[i])
+            {
+                return true;
+            }
         }
-        else if (a.getLL().getY() == b.getLL().getY() && a.getLL().getX() == b.getLL().getX())
-        {
-            return true;
-        }
-        else if (a.getLL().getY() == b.getLL().getY() && a.getLL().getX() == b.getLL().getX())
-        {
-            return true;
-        }
-        else if (a.getLL().getY() == b.getLL().getY() && a.getLL().getX() == b.getLL().getX())
-        {
-            return true;
-        }
-        else if (a.getLL().getY() == b.getLL().getY() && a.getLL().getX() == b.getLL().getX())
-        {
-            return true;
-        }
-        else if (a.getLL().getY() == b.getLL().getY() && a.getLL().getX() == b.getLL().getX())
-        {
-            return true;
-        }
+
+        return false;
     }
 }
